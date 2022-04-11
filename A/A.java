6831ee08b0572a885e2ba7bf3ff6736dc51ef7e2
kichan <ckcc21@gmail.com>
2022-04-11 15:20:54 +0900

    A 기능 추가

---
+++ A/A.java
@@ -0,0 +1,13 @@
+package A;
+
+import Main.CommonSeting;
+
+public class A implements CommonSeting {
+	public void whoname() {
+		System.out.println("장기찬");
+	}
+
+	public void 기능() {
+		System.out.println("A 기능");
+	}
+}
